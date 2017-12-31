0f9e18e35c59eecc77b8b3f585f4a7a1f18caf72
Behram ÇELEN <behramcelen@gmail.com>
2017-12-31 14:55:48 +0100

    revise selector functions

--- Lab5/Selectors.c
+++ Lab5/Selectors.c
@@ -1,67 +1,68 @@
 #include "Selectors.h"
 
-void run_with(bool (*mode)(const config *conf, const char *line), const config *conf) {
-    char line[LINE_LEN];
-
-    while (fgets(line, LINE_LEN, stdin)) {
-        if ((*mode)(conf, line)) {
-            printf("\t%s", line);
-        }
-    }
+void run_with(bool (*mode)(const config *conf), const config *conf) {
+    (*mode)(conf);
 }
 
-bool lineBegins(const config *conf, const char *line) {
+bool lineBegins(const config *conf) {
+    char line[LINE_LEN];
     char *pattern = conf->arg1;
 
-    return (strncmp(pattern, line, strlen(pattern)) == 0);
-}
-
-bool lineBeginsInRange(const config *conf, const char *line) {
-    char *from = conf->arg1;
-    char *to = conf->arg2;
-    if(strncmp(from, line, strlen(from)) == 0 || strncmp(to, line, strlen(to)) == 0){
-        return true;
+    while (fgets(line, LINE_LEN, stdin)) {
+        if (strncmp(pattern, line, strlen(pattern)) == 0) {
+            printf("%s", line);
+        }
     }
-
-    return false;
-}
-
-// Not implemented
-bool firstWordBeginsWith(const config *conf, const char *line) {
-    printf("\t%s", "begins with");
-    char *from = conf->arg1;
-    if(strncmp(from, line, strlen(from)) == 0){
-        return true;
-    }
-
-    return false;
-}
-
-// Not implemented
-bool firstWordEndsWith(const config *conf, const char *line) {
-    printf("\t%s", "ends with");
     return true;
 }
 
+bool lineBeginsInRange(const config *conf) {
+    char line[LINE_LEN];
+    char *from = conf->arg1;
+    char *to = conf->arg2;
 
-
-
-void get_first_word(const char *line, char *word, int len) {
-    int i;
-
-    for (i = 0; i < len - 1; ++i) {
-        if (isspace(line[i])) {
-            word[i] = 0;
-            break;
+    while (fgets(line, LINE_LEN, stdin)) {
+        if (strncmp(from, line, strlen(from)) == 0 || strncmp(to, line, strlen(to)) == 0) {
+            printf("%s", line);
         }
-
-        word[i] = line[i];
     }
 
-    // Overflow guard
-    if (i == len - 1) {
-        word[++i] = 0;
+    return false;
+}
+
+// Not implemented
+bool firstWordBeginsWith(const config *conf) {
+    char line[LINE_LEN];
+    char *to = conf->arg2;
+
+    while (fgets(line, LINE_LEN, stdin)) {
+        if (strncmp(to, line, strlen(to)) == 0) {
+            printf("%s", line);
+            return true;
+        }else{
+            printf("%s", line);
+        }
     }
+
+    return true;
+}
+
+// Not implemented
+bool firstWordEndsWith(const config *conf) {
+    char line[LINE_LEN];
+    char *from = conf->arg1;
+    bool started;
+
+    while (fgets(line, LINE_LEN, stdin)) {
+        if (strncmp(from, line, strlen(from)) == 0) {
+            started = true;
+        }
+        if(started){
+            printf("%s", line);
+        }
+    }
+
+    return true;
 }
 
 bool in_range(const int num, const int begin, const int end) {
--- Lab5/Selectors.h
+++ Lab5/Selectors.h
@@ -10,17 +10,15 @@
 #include "Consts.h"
 
 
-void run_with(bool (*mode)(const config *conf, const char *line), const config *conf);
+void run_with(bool (*mode)(const config *conf), const config *conf);
 
-bool lineBegins(const config *conf, char *inputGets);
+bool lineBegins(const config *conf);
 
-bool lineBeginsInRange(const config *conf, const char *line);
+bool lineBeginsInRange(const config *conf);
 
-bool firstWordBeginsWith(const config *conf, const char *line);
+bool firstWordBeginsWith(const config *conf);
 
-bool firstWordEndsWith(const config *conf, const char *line);
-
-void get_first_word(const char *line, char *word, int len);
+bool firstWordEndsWith(const config *conf);
 
 bool in_range(const int num, const int begin, const int end);
 
